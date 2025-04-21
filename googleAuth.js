0d7235525e66ac78d77e057bfb54a00432481483
Ashutosh Gupta <154120097+ashutoshSieve@users.noreply.github.com>
2025-04-21 16:51:03 +0530

    Update googleAuth.js

--- googleAuth.js
+++ googleAuth.js
@@ -7,7 +7,7 @@ const passport=require("passport");
 passport.use(new GoogleStrategy({
     clientID: process.env.ID,
     clientSecret: process.env.SECRET,
-    callbackURL: "https://expensetracker-93re.onrender.com/auth/callback"
+    callbackURL: "https://driving-elspeth-ashutoshserver-54c4997a.koyeb.app/auth/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
     UserModel.findOne({google_id: profile.id }).then(user => {
